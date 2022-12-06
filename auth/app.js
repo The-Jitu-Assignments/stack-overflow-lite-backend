360e3462edd52900ef62b5626390aa9a9379c447
john katua <johnkatua99@gmail.com>
2022-12-06 13:28:16 +0300

    using port from dotenv file

--- auth/app.js
+++ auth/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
 const userRoutes = require('./routes/users/users');
+const dotenv = require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/', userRoutes);
 
 app.get('/', (req, res) => {
   res.send('welcome')
 });
 
-app.listen(3000, () => {
-  console.log('Server up and running')
+app.listen(process.env.PORT, () => {
+  console.log(`Auth Service running on port: ${process.env.PORT}`)
 })
