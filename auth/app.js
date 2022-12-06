a383ee02977111cfde9984e799bc706ba2208bac
john katua <johnkatua99@gmail.com>
2022-12-06 05:53:39 +0300

    auth service created

---
+++ auth/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('welcome')
+});
+
+app.listen(3000, () => {
+  console.log('Server up and running')
+})
