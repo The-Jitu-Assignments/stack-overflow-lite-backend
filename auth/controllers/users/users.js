81e690fd0fb7b6d107e1cd9b3c9a39f918f7e336
john katua <johnkatua99@gmail.com>
2022-12-06 13:52:25 +0300

    installed uuid and bcrypt

--- auth/controllers/users/users.js
+++ auth/controllers/users/users.js
@@ -1,3 +1,7 @@
+const { v4 } = require('uuid');
+const sql = require('mssql');
+const sqlConfig = require('../../config/index');
+
 exports.signup = async () => {};
 
 exports.login = async () => {};
