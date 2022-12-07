a1a8c49a804f280acef8ed589c6f144dcd5d68ef
john katua <johnkatua99@gmail.com>
2022-12-07 05:30:47 +0300

    user can logged in successfully

--- auth/controllers/users/users.js
+++ auth/controllers/users/users.js
@@ -1,7 +1,10 @@
 const { v4 } = require('uuid');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const sql = require('mssql');
 const sqlConfig = require('../../config/index');
+const { SECRET_KEY } = process.env;
+const dotenv = require('dotenv').config();
 
 exports.signup = async (req, res) => {
   try {
@@ -41,9 +44,34 @@ exports.login = async (req, res) => {
     if (!email || !password) {
       return res.status(400).json({ msg: 'Please Provide all details' })
     }
+
     const pool = await sql.connect(sqlConfig);
 
+    const getUser = await pool.request()
+      .input('email', email)
+    .execute('usp_getUser');
+
+    const user = getUser.recordset[0];
+
+    if (user) {
+      const checkPassword = await bcrypt.compare(password, user.password);
+      if(checkPassword) {
+        const { password, ...payload } = user;
+        const token = jwt.sign(payload, SECRET_KEY, { expiresIn: '24h' });
+        return res.status(200).json({
+          msg: 'User logged in successfully',
+          token
+        })
+      } else {
+        return res.status(400).json({ msg: 'Password do not match' });
+      }
+    } else {
+      return res.status(404).json({ msg: 'User with that email does not exist' });
+    }
+
   } catch (error) {
-    
+    return res.status(500).json({
+      msg: error
+    })
   }
 };
