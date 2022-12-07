75176d18d0db5ff48d8a058dc8742a4660bb3ab4
john katua <johnkatua99@gmail.com>
2022-12-07 17:28:41 +0300

    added a route to create an answer

--- quiz/app.js
+++ quiz/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv').config();
 const { PORT } = process.env;
 const questionsRoutes = require('./routes/questions/Questions');
+const answersRoutes = require('./routes/answers/Answers');
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/question', questionsRoutes);
+app.use('/answer', answersRoutes);
 
 app.listen(PORT, () => {
   console.log(`Quiz service is running on port: ${PORT}`)
