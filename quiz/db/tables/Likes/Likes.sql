7ac82aec9fc6e3f322c35df05b7c34ecb9364ca4
john katua <johnkatua99@gmail.com>
2022-12-09 05:46:14 +0300

    updated the liketable

--- quiz/db/tables/Likes/Likes.sql
+++ quiz/db/tables/Likes/Likes.sql
@@ -1,6 +1,7 @@
 CREATE TABLE LikeTable (
-  id VARCHAR(255) PRIMARY KEY,
+  id VARCHAR(255),
   answerId VARCHAR(255) FOREIGN KEY REFERENCES Answers(id),
   userId VARCHAR(255) FOREIGN KEY REFERENCES Users(id),
-  likes INT DEFAULT 0
+  likes INT DEFAULT 0,
+  CONSTRAINT PK_Like PRIMARY KEY (answerId, userId)
 );
