d80a0ba92e90832b6b781a0afbf68a870114660d
john katua <johnkatua99@gmail.com>
2022-12-09 11:03:40 +0300

    added a controller to add likes to answers

--- quiz/controllers/Likes/Likes.js
+++ quiz/controllers/Likes/Likes.js
@@ -1,21 +1,27 @@
-// const sql = require('mssql');
-// const sqlConfig = require('../../config');
-// const { v4 } = require('uuid');
+const sql = require('mssql');
+const sqlConfig = require('../../config');
+const { v4 } = require('uuid');
 
-// exports.addLike = async (req, res) => {
-//   try {
-//     const { answerId, likes } = req.body;
-//     const { currentUser } = req.info;
+exports.addLike = async (req, res) => {
+  try {
+    const { answerId, likes } = req.body;
+    const { currentUser } = req.info;
 
-//     const pool = await sql.connect(sqlConfig);
+    const pool = await sql.connect(sqlConfig);
 
-//     await pool.request()
-//       .input('id', v4())
-//       .input('answerId', answerId)
-//       .input('userId', currentUser)
-//       .input('likes', likes)
-//     .execute('')
-//   } catch (error) {
-    
-//   }
-// }
+    await pool.request()
+      .input('id', v4())
+      .input('answerId', answerId)
+      .input('userId', currentUser)
+      .input('likes', likes)
+    .execute('usp_addLike');
+
+    return res.status(201).json({
+      msg: 'Like added successfully'
+    })
+  } catch (error) {
+    return res.status(500).json({
+      msg: error
+    })
+  }
+}
