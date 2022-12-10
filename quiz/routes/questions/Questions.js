b208d8be053c252faaaa09147f60110803f7704f
john katua <johnkatua99@gmail.com>
2022-12-10 13:59:35 +0300

    updated the router to search questions from the database

--- quiz/routes/questions/Questions.js
+++ quiz/routes/questions/Questions.js
@@ -9,7 +9,7 @@ router.get('/', questionsController.getQuestions);
 router.get('/myQuestions', verifyToken, questionsController.getMyQuestions);
 router.get('/:id', questionsController.getQuestion);
 router.delete('/:id', verifyToken, questionsController.deleteQuestion);
-router.get('/searchQn', questionsController.findQuestions);
+router.get('/quiz/searchQn', questionsController.findQuestions);
 router.get('/qn/mostAnsweredQn', questionsController.getMostAnsweredQn);
 
 
