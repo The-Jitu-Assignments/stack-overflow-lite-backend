12332a98b2c1a7d09a9b5e2ad06e83064480a965
john katua <johnkatua99@gmail.com>
2022-12-11 10:28:09 +0300

    add a token verification middleware to the create profile middleware

--- auth/routes/profile/Profile.js
+++ auth/routes/profile/Profile.js
@@ -1,7 +1,8 @@
 const { Router } =  require('express');
 const profileController = require('../../controllers/profile/Profile');
+const { verifyToken } = require('../../middleware/verifyToken');
 const router = Router();
 
-router.post('/', profileController.createProfile);
+router.post('/', verifyToken, profileController.createProfile);
 
 module.exports = router;
