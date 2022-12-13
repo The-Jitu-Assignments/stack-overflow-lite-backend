29daaeae0b42606ff8e2482ed339a88a3c025f30
john katua <johnkatua99@gmail.com>
2022-12-13 10:19:42 +0300

    updated the procedure to fetch all questions

--- quiz/db/storedProcedures/Questions/getQuestions.sql
+++ quiz/db/storedProcedures/Questions/getQuestions.sql
@@ -1,5 +1,7 @@
 CREATE OR ALTER PROC usp_getAllQuestions
 AS
 BEGIN
- SELECT * FROM Questions;
+  SELECT 
+    q.id, q.userId, q.question, q.date, p.phone, p.address, p.github, p.imgUrl
+  FROM Questions q LEFT JOIN Profile p ON q.userId = p.userId;
 END;
