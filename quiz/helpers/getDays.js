49411dd54161cfb8e2c967207bfd7421415e5819
john katua <johnkatua99@gmail.com>
2022-12-14 10:08:26 +0300

    added a helper fn to extract number of days from date

---
+++ quiz/helpers/getDays.js
@@ -0,0 +1,21 @@
+exports.getDays = (data) => {
+  data.map(item => {
+    let todaysDate = new Date();
+    let itemDate = item.date;
+    let diffTime = Math.ceil((todaysDate - itemDate) / (1000 * 60 * 60 * 24));
+    return {
+      ...item,
+      days: `${diffTime > 1 ? `${diffTime} days`: `${diffTime} day`}`
+    }
+  })
+}
+
+// let newData = questions.map(qn => {
+//       questionId = qn.id;
+//       let todaysDate = new Date();
+//       let qnDate = qn.date;
+//       let diffTime = Math.ceil((todaysDate - qnDate) / (1000 * 60 * 60 * 24))
+//       return {
+//         ...qn,
+        
+//       }
