7ce69ec1fb561a79b63e0539a7d7b53096ee8fe1
john katua <johnkatua99@gmail.com>
2022-12-16 11:52:18 +0300

    updated the functionality to get user profile

--- auth/db/storedProcedures/profile/getProfile.sql
+++ auth/db/storedProcedures/profile/getProfile.sql
@@ -1,7 +1,7 @@
 CREATE OR ALTER PROC usp_getUserProfile(@userId VARCHAR(255))
 AS
 BEGIN
-  select 
-    u.id, u.name, u.email, p.id as profileId, p.phone, p.address, p.github, p.imgUrl 
-  from Users u LEFT JOIN Profile p ON u.id = p.userId WHERE u.id = @userId;
+  SELECT 
+  id, name, email 
+  FROM Users WHERE id = @userId;
 END;
