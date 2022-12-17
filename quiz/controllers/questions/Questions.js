dc2ff9de72f97ba8dc25b113a246f3440fd392a6
john katua <johnkatua99@gmail.com>
2022-12-17 12:15:25 +0300

    added a controller to get recently asked questions

--- quiz/controllers/questions/Questions.js
+++ quiz/controllers/questions/Questions.js
@@ -191,4 +191,25 @@ exports.getMostAnsweredQn = async (req, res) => {
       msg: error
     })
   }
+};
+
+exports.getRecentQuestions = async (req, res) => {
+  try {
+    const questions = await (await execute('usp_getMostRecentQuizes')).recordset;
+
+    if (questions) {
+      return res.status(200).json({
+        msg: 'Questions fetched successfully',
+        data: questions
+      })
+    } else {
+      return res.status(404).json({
+        msg: 'Not questions were found'
+      })
+    }
+  } catch (error) {
+    return res.status(500).json({
+      msg: error
+    })
+  }
 }
