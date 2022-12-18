cf6a8b90d9c1720bfa5c3f4c5469aadeb35c6eb9
john katua <johnkatua99@gmail.com>
2022-12-18 13:40:41 +0300

    updated the router to get questions belonging to a specific user

--- quiz/routes/questions/Questions.js
+++ quiz/routes/questions/Questions.js
@@ -7,7 +7,7 @@ const router = Router();
 router.post('/', verifyToken, questionsController.createQuestion);
 router.get('/', questionsController.getQuestions);
 router.get('/recent',questionsController.getRecentQuestions);
-router.get('/myQuestions', verifyToken, questionsController.getMyQuestions);
+router.get('/myQuestions/:id', questionsController.getMyQuestions);
 router.get('/:id', questionsController.getQuestion);
 router.delete('/:id', verifyToken, questionsController.deleteQuestion);
 router.get('/quiz/searchQn', questionsController.findQuestions);
