2aa673e770f1831f8c541bc8547ddb0a4d43a1db
john katua <johnkatua99@gmail.com>
2022-12-18 23:52:41 +0300

    updated the procedure to get answers to a particular question

--- quiz/db/storedProcedures/Answers/getAnswer.sql
+++ quiz/db/storedProcedures/Answers/getAnswer.sql
@@ -1,5 +1,10 @@
 CREATE OR ALTER PROC usp_getAnswer(@answerId VARCHAR(255))
 AS
 BEGIN
-  SELECT * FROM Answers WHERE id = @answerId
+  SELECT 
+    *,
+    (SELECT COUNT(c.answerId) FROM Comments c WHERE a.id = c.answerId) totalComments,
+    (SELECT COUNT(ld.answerId) FROM likeDislikes ld WHERE a.id = ld.answerId AND ld.total = 1) totalLikes,
+    (SELECT COUNT(ld.answerId) FROM likeDislikes ld WHERE a.id = ld.answerId AND ld.total = -1) totalDislikes
+  FROM Answers a WHERE a.id = @answerId; 
 END;
