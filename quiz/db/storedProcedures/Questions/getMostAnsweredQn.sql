8cdd0cfbf161a1365d6825fad2690b5755fc70e9
john katua <johnkatua99@gmail.com>
2022-12-18 07:06:49 +0300

    updated the stored proc to get most answered question

--- quiz/db/storedProcedures/Questions/getMostAnsweredQn.sql
+++ quiz/db/storedProcedures/Questions/getMostAnsweredQn.sql
@@ -1,5 +1,5 @@
-CREATE OR ALTER PROC usp_mostAnsweredQuestion (@range INT)
+CREATE OR ALTER PROC usp_mostAnsweredQuestion
 AS
 BEGIN
-  SELECT q.id from Questions q LEFT JOIN Answers a ON q.id = a.questionId GROUP BY q.id HAVING count(a.questionId) > @range
+ select q.id, count(a.questionId) as total from Questions q LEFT JOIN Answers a ON q.id = a.questionId GROUP BY q.id HAVING count(a.questionId) >= 1 ORDER BY count(a.questionId) DESC;
 END;
