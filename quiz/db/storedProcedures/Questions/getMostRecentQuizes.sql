ea30cbb97b7c53268c4a613b5d1eb07192f489d7
john katua <johnkatua99@gmail.com>
2022-12-19 06:23:14 +0300

    updated the controller to get most recent questions

--- quiz/db/storedProcedures/Questions/getMostRecentQuizes.sql
+++ quiz/db/storedProcedures/Questions/getMostRecentQuizes.sql
@@ -2,6 +2,7 @@ CREATE OR ALTER PROC usp_getMostRecentQuizes
 AS
 BEGIN
   SELECT 
-    q.id, q.userId, q.question, q.date, p.imgUrl, u.name
+    q.id, q.userId, q.question, q.date, p.imgUrl, u.name,
+    (select count(a.questionId) from Answers a WHERE a.questionId = q.id) totalAns
   FROM Questions q LEFT JOIN Profile p ON q.userId = p.userId LEFT JOIN Users u ON u.id = q.userId order by q.date desc;
 END;
