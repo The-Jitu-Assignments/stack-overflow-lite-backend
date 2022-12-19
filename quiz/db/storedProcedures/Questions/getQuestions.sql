9f56cb037b0e0f404a14e362469bcdf91f326a84
john katua <johnkatua99@gmail.com>
2022-12-19 11:11:47 +0300

    updated the proc to get all questions

--- quiz/db/storedProcedures/Questions/getQuestions.sql
+++ quiz/db/storedProcedures/Questions/getQuestions.sql
@@ -1,10 +1,5 @@
-CREATE OR ALTER PROC usp_getAllQuestions
+CREATE OR ALTER PROC usp_getAllQuestions(@pageNumber INT, @pageSize INT)
 AS
 BEGIN
-  SELECT 
-    q.id, q.userId, q.question, q.date, p.imgUrl, u.name, 
-    (select count(a.questionId) from Answers a WHERE a.questionId = q.id) totalAns
-    FROM Questions q LEFT JOIN Profile p 
-    ON q.userId = p.userId LEFT JOIN Users u 
-  ON u.id = q.userId
-END;
+  SELECT * FROM udf_getPaginatedData(@pageNumber, @pageSize)
+END
