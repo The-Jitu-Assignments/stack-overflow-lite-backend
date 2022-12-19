e068ca33f103fb38f65e452c000b1d8abdce158b
john katua <johnkatua99@gmail.com>
2022-12-19 06:03:38 +0300

    updatedd the procedure to find my questions

--- quiz/db/storedProcedures/Questions/getMyQuestions.sql
+++ quiz/db/storedProcedures/Questions/getMyQuestions.sql
@@ -2,7 +2,9 @@ CREATE OR ALTER PROC usp_findMyQuestions(@userId VARCHAR(255))
 AS
 BEGIN
  SELECT 
-    q.id, q.userId, q.question, q.date, u.name, p.imgUrl FROM Questions q 
+    q.id, q.userId, q.question, q.date, u.name, p.imgUrl,
+    (select count(a.questionId) from Answers a WHERE a.questionId = q.id) totalAns
+    FROM Questions q 
     LEFT JOIN Users u ON u.id = q.userId 
     LEFT JOIN Profile p ON u.id = p.userId
   WHERE q.userId = @userId
