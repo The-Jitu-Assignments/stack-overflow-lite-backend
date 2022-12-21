6d4f6aa9d4e0655234943c6553ed81063425a70e
john katua <johnkatua99@gmail.com>
2022-12-21 08:43:00 +0300

    updated the stored procedure to fetch user profile

--- auth/db/storedProcedures/profile/getProfile.sql
+++ auth/db/storedProcedures/profile/getProfile.sql
@@ -2,6 +2,9 @@ CREATE OR ALTER PROC usp_getUserProfile(@userId VARCHAR(255))
 AS
 BEGIN
   select 
-    u.id, u.name, u.email, p.id as profileId, p.phone, p.address, p.github, p.imgUrl 
-  from Users u LEFT JOIN Profile p ON u.id = p.userId WHERE u.id = @userId;
+    u.id, u.name, u.email, p.id as profileId, p.phone, p.address, p.github, p.imgUrl,
+  (select count(a.id) from Answers a WHERE a.userId = u.id) totalAnswersGiven,
+  (select count(q.id) from Questions q WHERE q.userId = u.id) totalQuestionsAsked 
+  from Users u LEFT JOIN Profile p ON u.id = p.userId
+   WHERE u.id = @userId;
 END;
